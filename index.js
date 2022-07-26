f2d8db5ac22cf7d3fd97502b2faf6bfcaec76e2a
Swapnil Mishra <79589558+Swapnil250102@users.noreply.github.com>
2022-07-26 18:09:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var bar = document.getElementById('bar');
+var close = document.getElementById('close');
+var nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
+}
